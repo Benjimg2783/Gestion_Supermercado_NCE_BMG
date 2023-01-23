b3be4834785c17c0b466d6d5072829cf5a3c3e46
Benjamin MG <bmargac529@g.educaand.es>
2023-01-23 19:47:50 +0100

    Initial commit

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.util.List;
+
+public class Cliente extends Persona{
+
+    // Atributos de la clase
+    private double dinero;
+    private List<Producto> listaCompra;
+
+
+    // Constructor
+    public Cliente(String dni,
+                  String nombre,
+                  int edad,
+                  char sexo,
+                  String direccion,
+                  double dinero,
+                   List<Producto> listaCompra) {
+
+        this.dinero=dinero;
+        this.listaCompra=listaCompra;
+        super.dni = dni;
+        super.nombre = nombre;
+        super.edad = edad;
+        super.sexo = sexo;
+        super.direccion = direccion;
+    }
+
+
+    // Getter y setters
+
+    public double getdinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero){
+        this.dinero=dinero;
+    }
+
+    public List<Producto> getListaDeLaCompra() {
+        return listaCompra;
+    }
+
+    public void setListaDeLaCompra(List<Producto> listaDeLaCompra) {
+        this.listaCompra = listaDeLaCompra;
+    }
+
+    // Método toString
+
+    @Override
+    public String toString() {
+        return super.toString() +  ", Lista de la compra = " + listaCompra
+                +  ", dinero = " + dinero;
+    }
+
+    //Suma dinero al cliente
+    public void sumDinero(double dineroIngresado) {
+        this.dinero += dineroIngresado;
+    }
+
+    //Resta dinero al cliente
+    public void restaDinero(double dineroExtraido) {
+        this.dinero += dineroExtraido;
+    }
+
+    public void añadirProductoALaLista(Producto producto){
+        this.listaCompra.add(producto);
+    }
+    public void eliminarProductoDeLaLista(Producto producto){
+        int count=0;
+        for (int i = 0; i <= this.listaCompra.size(); i++){
+            if (this.listaCompra.get(i) ==producto)break;
+            else count++;
+        }
+        if (count>this.listaCompra.size()) System.out.println("elemento no encontrado");
+        else this.listaCompra.remove(count);
+    }
+}
---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public abstract class Persona {
+
+    // Atributos de la clase
+    protected String dni;
+    protected String nombre;
+    protected int edad;
+    protected char sexo;
+    protected String direccion;
+
+
+    // Getter y setters
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+
+        if (sexo == 'H' || sexo == 'M'){
+            this.sexo = sexo;
+        } else sexo = 'H';
+
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return " DNI = " + dni +
+                ", nombre = " + nombre +
+                ", edad = " + edad +
+                ", sexo = " + sexo +
+                ", direccion = " + direccion ;
+    }
+}
